9eb2554267c977b1971747e25b37b1610e5ec564
Mohamed Amjad LASRI <amjad.sig@gmail.com>
2018-01-07 16:51:47 +0100

    structuring th code

--- setup.py
+++ setup.py
@@ -8,18 +8,18 @@ def readme():
 setup(
   name='cryptocurrenciestools',
   packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests*']), # this must be the same as the name above
-  version='0.1',
+  version='0.2dev',
   description='Download and analyse cryptocurrencies data',
   long_description=readme(),
   author='Mohamed Amjad LASRI',
   author_email='amjad.sig@gmail.com',
   url='https://github.com/mohamedamjad/CMC_data_sniffer', # use the URL to the github repo
-  download_url='https://github.com/mohamedamjad/CMC_data_sniffer/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url='https://github.com/mohamedamjad/CMC_data_sniffer/archive/0.2dev.tar.gz', # I'll explain this in a second
   keywords=['cryptocurrency', 'analysis', 'tools', 'bitcoin', 'data'], # arbitrary keywords
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Programming Language :: Python :: 2.7',
   ],
   install_requires=['requests','pandas'],
-  python_requires='>=3',
+  python_requires='>=2.6',
 )
